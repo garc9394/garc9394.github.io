d7678103e47618c5cd304a03298847088a9ea52e
Gar Cheuy <garcheuy@yahoo.com>
2019-02-05 08:57:42 -0800

    Final

---
+++ data - Copy.js
@@ -0,0 +1,129 @@
+function projects() {
+    var a = document.getElementById("contents");
+    var b = document.getElementById("bio");
+    var c = document.getElementById("aliens");
+    var d = document.getElementById("latitude");
+    var e = document.getElementById("mvp");
+    var f = document.getElementById("sentiment");
+    var g = document.getElementById("bike");
+}
+
+function bio() {
+    var a = document.getElementById("contents");
+    var b = document.getElementById("bio");
+    var c = document.getElementById("aliens");
+    var d = document.getElementById("latitude");
+    var e = document.getElementById("mvp");
+    var f = document.getElementById("sentiment");
+    var g = document.getElementById("bike");
+    var h = document.getElementById("photo_credits");
+    b.style.display = "block";
+
+    a.style.display = "none";
+    c.style.display = "none";
+    d.style.display = "none";
+    e.style.display = "none";
+    f.style.display = "none";
+    g.style.display = "none";
+    h.style.display = "none";
+}
+
+function aliens() {
+    var a = document.getElementById("contents");
+    var b = document.getElementById("bio");
+    var c = document.getElementById("aliens");
+    var d = document.getElementById("latitude");
+    var e = document.getElementById("mvp");
+    var f = document.getElementById("sentiment");
+    var g = document.getElementById("bike");
+    var h = document.getElementById("photo_credits");
+    c.style.display = "block";
+
+    a.style.display = "none";
+    b.style.display = "none";
+    d.style.display = "none";
+    e.style.display = "none";
+    f.style.display = "none";
+    g.style.display = "none";
+    h.style.display = "none";
+}
+
+function latitude() {
+    var a = document.getElementById("contents");
+    var b = document.getElementById("bio");
+    var c = document.getElementById("aliens");
+    var d = document.getElementById("latitude");
+    var e = document.getElementById("mvp");
+    var f = document.getElementById("sentiment");
+    var g = document.getElementById("bike");
+    var h = document.getElementById("photo_credits");
+    d.style.display = "block";
+
+    a.style.display = "none";
+    b.style.display = "none";
+    c.style.display = "none";
+    e.style.display = "none";
+    f.style.display = "none";
+    g.style.display = "none";
+    h.style.display = "none";
+}
+
+function mvp() {
+    var a = document.getElementById("contents");
+    var b = document.getElementById("bio");
+    var c = document.getElementById("aliens");
+    var d = document.getElementById("latitude");
+    var e = document.getElementById("mvp");
+    var f = document.getElementById("sentiment");
+    var g = document.getElementById("bike");
+    var h = document.getElementById("photo_credits");
+    e.style.display = "block";
+
+    a.style.display = "none";
+    b.style.display = "none";
+    c.style.display = "none";
+    d.style.display = "none";
+    f.style.display = "none";
+    g.style.display = "none";
+    h.style.display = "none";
+}
+
+function sentiment() {
+    var a = document.getElementById("contents");
+    var b = document.getElementById("bio");
+    var c = document.getElementById("aliens");
+    var d = document.getElementById("latitude");
+    var e = document.getElementById("mvp");
+    var f = document.getElementById("sentiment");
+    var g = document.getElementById("bike");
+    var h = document.getElementById("photo_credits");
+    f.style.display = "block";
+
+    a.style.display = "none";
+    b.style.display = "none";
+    c.style.display = "none";
+    d.style.display = "none";
+    e.style.display = "none";
+    g.style.display = "none";
+    h.style.display = "none";
+}
+
+function bike() {
+    var a = document.getElementById("contents");
+    var b = document.getElementById("bio");
+    var c = document.getElementById("aliens");
+    var d = document.getElementById("latitude");
+    var e = document.getElementById("mvp");
+    var f = document.getElementById("sentiment");
+    var g = document.getElementById("bike");
+    var h = document.getElementById("photo_credits");
+    g.style.display = "block";
+
+    a.style.display = "none";
+    b.style.display = "none";
+    c.style.display = "none";
+    d.style.display = "none";
+    e.style.display = "none";
+    f.style.display = "none";
+    h.style.display = "none";
+}
--- data.js
+++ data.js
@@ -3,9 +3,6 @@ function projects() {
     var b = document.getElementById("bio");
     var c = document.getElementById("aliens");
     var d = document.getElementById("latitude");
-    var e = document.getElementById("mvp");
-    var f = document.getElementById("sentiment");
-    var g = document.getElementById("bike");
 }
 
 function bio() {
@@ -13,19 +10,13 @@ function bio() {
     var b = document.getElementById("bio");
     var c = document.getElementById("aliens");
     var d = document.getElementById("latitude");
-    var e = document.getElementById("mvp");
-    var f = document.getElementById("sentiment");
-    var g = document.getElementById("bike");
-    var h = document.getElementById("photo_credits");
+    var e = document.getElementById("photo_credits");
     b.style.display = "block";
 
     a.style.display = "none";
     c.style.display = "none";
     d.style.display = "none";
     e.style.display = "none";
-    f.style.display = "none";
-    g.style.display = "none";
-    h.style.display = "none";
 }
 
 function aliens() {
@@ -33,19 +24,13 @@ function aliens() {
     var b = document.getElementById("bio");
     var c = document.getElementById("aliens");
     var d = document.getElementById("latitude");
-    var e = document.getElementById("mvp");
-    var f = document.getElementById("sentiment");
-    var g = document.getElementById("bike");
-    var h = document.getElementById("photo_credits");
+    var e = document.getElementById("photo_credits");
     c.style.display = "block";
 
     a.style.display = "none";
     b.style.display = "none";
     d.style.display = "none";
     e.style.display = "none";
-    f.style.display = "none";
-    g.style.display = "none";
-    h.style.display = "none";
 }
 
 function latitude() {
@@ -53,77 +38,11 @@ function latitude() {
     var b = document.getElementById("bio");
     var c = document.getElementById("aliens");
     var d = document.getElementById("latitude");
-    var e = document.getElementById("mvp");
-    var f = document.getElementById("sentiment");
-    var g = document.getElementById("bike");
-    var h = document.getElementById("photo_credits");
+    var e = document.getElementById("photo_credits");
     d.style.display = "block";
 
     a.style.display = "none";
     b.style.display = "none";
     c.style.display = "none";
     e.style.display = "none";
-    f.style.display = "none";
-    g.style.display = "none";
-    h.style.display = "none";
-}
-
-function mvp() {
-    var a = document.getElementById("contents");
-    var b = document.getElementById("bio");
-    var c = document.getElementById("aliens");
-    var d = document.getElementById("latitude");
-    var e = document.getElementById("mvp");
-    var f = document.getElementById("sentiment");
-    var g = document.getElementById("bike");
-    var h = document.getElementById("photo_credits");
-    e.style.display = "block";
-
-    a.style.display = "none";
-    b.style.display = "none";
-    c.style.display = "none";
-    d.style.display = "none";
-    f.style.display = "none";
-    g.style.display = "none";
-    h.style.display = "none";
-}
-
-function sentiment() {
-    var a = document.getElementById("contents");
-    var b = document.getElementById("bio");
-    var c = document.getElementById("aliens");
-    var d = document.getElementById("latitude");
-    var e = document.getElementById("mvp");
-    var f = document.getElementById("sentiment");
-    var g = document.getElementById("bike");
-    var h = document.getElementById("photo_credits");
-    f.style.display = "block";
-
-    a.style.display = "none";
-    b.style.display = "none";
-    c.style.display = "none";
-    d.style.display = "none";
-    e.style.display = "none";
-    g.style.display = "none";
-    h.style.display = "none";
-}
-
-function bike() {
-    var a = document.getElementById("contents");
-    var b = document.getElementById("bio");
-    var c = document.getElementById("aliens");
-    var d = document.getElementById("latitude");
-    var e = document.getElementById("mvp");
-    var f = document.getElementById("sentiment");
-    var g = document.getElementById("bike");
-    var h = document.getElementById("photo_credits");
-    g.style.display = "block";
-
-    a.style.display = "none";
-    b.style.display = "none";
-    c.style.display = "none";
-    d.style.display = "none";
-    e.style.display = "none";
-    f.style.display = "none";
-    h.style.display = "none";
 }
